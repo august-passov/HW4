d691c56bb44574b93f5249fdfb1e18807c04b20d
august passov <gus.passov@kellogg.northwestern.edu>
2023-05-30 15:50:47 +0000

    HW4 in progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+ 
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
@@ -6,13 +10,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+    if @current_user
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post.save
+    else
+    flash["notice"] = "Login First."
+    end
+    redirect_to "/places"
   end
 
 end
